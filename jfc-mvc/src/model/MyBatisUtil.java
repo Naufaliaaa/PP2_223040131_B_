b443273dbef8a54ce9246b2eb7ae8a4f3eb3acd5
Naufaliaaa <naufalzul47@gmail.com>
2024-12-19 16:00:44 +0700

    MVC

---
+++ jfc-mvc/src/model/MyBatisUtil.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Naufal Zul Faza
+ */
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.*;
+import java.io.IOException;
+
+public class MyBatisUtil {
+    private static SqlSessionFactory sqlSessionFactory;
+    static {
+        try {
+            sqlSessionFactory = new SqlSessionFactoryBuilder()
+                .build(Resources.getResourceAsStream("mybatis-config.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error initializing SqlSessionFactory.");
+        }
+    }
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
+    }
+}
